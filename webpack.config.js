382ea6e40a510ccf006456541c608c93d99cb816
Eldar Khaitov <eldar.khaitov@gmail.com>
2017-04-22 06:40:13 +0300

    can build js and scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,46 @@
-var path = require('path');
+const path = require('path');
+const live = process.env.NODE_ENV === "production";
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+  filename: "[name].css",
+  disable: process.env.NODE_ENV === "development"
+});
 
 module.exports = {
-  entry: './app/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        // loaders: ["style-loader", "css-loader", "sass-loader"],
+        use: extractSass.extract({
+          use: [{
+            loader: "css-loader"
+          }, {
+            loader: "sass-loader"
+          }],
+          // use style-loader in development
+          fallback: "style-loader"
+        })
+      },
+      {
+        test: /\.(png|jpeg|ttf|...)$/,
+        use: [
+          {loader: 'url-loader', options: {limit: 8192}}
+          // limit => file.size =< 8192 bytes ? DataURI : File
+        ]
+      }
+    ]
+  },
+  plugins: [
+    extractSass
+  ],
+  entry: [
+    path.join(__dirname, "app", "index.js"),
+    path.join(__dirname, "app", "app.scss"),
+  ]
 };
